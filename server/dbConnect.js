bcb5e10587c68fc66f460e96433e0b8b07c58699
KarateMan89 <slavchev89@hotmail.com>
2023-12-10 01:50:06 -0500

    works will continue tomorrow

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,22 +1,22 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'dojoDB',
-  password: 'postgres',
-  port: 5432,
-  max: 3,
-  idleTimeoutMillis: 30000,
+    user: "postgres",
+    host: "localhost",
+    database: "dojoDB", // change to your database name
+    password: "kyokushin", // change to your password
+    port: 5432,
+    max: 3,
+    idleTimeoutMillis: 30000,
 });
 
 db.connect((err, client, release) => {
-  if (err) return console.error('Error acquiring client', err.stack);
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) return console.error('Error executing query', err.stack);
-    console.log('PostgreSQL connection successful', result.rows);
-  });
+    if (err) return console.error("Error acquiring client", err.stack);
+    client.query("SELECT NOW()", (err, result) => {
+        release();
+        if (err) return console.error("Error executing query", err.stack);
+        console.log("PostgreSQL connection successful", result.rows);
+    });
 });
 
 module.exports = db;
